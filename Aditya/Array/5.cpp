19c69d1aa95c2a6681b90309c8cfab13b34a9c36
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-31 09:45:15 +0530

    q5

--- Aditya/Array/5.cpp
+++ Aditya/Array/5.cpp
@@ -3,9 +3,9 @@
  * Insert all elements of b into mySet
  * As there are no duplicate elements in the set, return
  * its size. That is the answer.
-
 */
 
+#include <iostream>
 #include <set>
 using namespace std;
 
@@ -18,4 +18,20 @@ int doUnion(int a[], int n, int b[], int m)  {
         mySet.insert(b[i]);
     }
     return mySet.size();
+}
+
+void doIntersection(int a[], int n, int b[], int m){
+    int i = 0, j = 0;
+    while (i < n && j < m) {
+        if (a[i] > b[j]) {
+            j++;
+        } else if (b[j] > a[i]) {
+            i++;
+        } else {
+            // when both are equal
+            cout<<a[i]<<" ";
+            i++;
+            j++;
+        }
+    }
 }
