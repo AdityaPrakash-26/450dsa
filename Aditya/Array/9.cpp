6fd8ea4411f053739e2b7efc8033ae182dfdab2e
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-31 12:03:34 +0530

    q9

--- Aditya/Array/9.cpp
+++ Aditya/Array/9.cpp
@@ -1,53 +1,53 @@
 int minJumps(int arr[], int n){
-        if(n<=1){   //already at the end
-            return 0;
-        }
-        
-        if(arr[0]==0){  //can't go anywhere
-            return -1;
-        }
-        
-        // stores the maximal
-        // reachable index in the array.
-        int maxReach = arr[0];
-    
-        // stores the number of steps
-        // we can still take
-        int step = arr[0];
-    
-        // stores the number of jumps
-        // necessary to reach that maximal
-        // reachable position.
-        int jump = 1;
-        
-        int i = 1;
-        for (i = 1; i < n; i++) {
-            // Check if we have reached the end of the array
-            if (i == n - 1)
-                return jump;
-    
-            // updating maxReach
-            maxReach = max(maxReach, i + arr[i]);
-    
-            // we use a step to get to the current index
-            step--;
-    
-            // If no further steps left
-            if (step == 0) {
-                // we must have used a jump
-                jump++;
-    
-                // Check if the current index/position or lesser index
-                // is the maximum reach point from the previous indexes
-                if (i >= maxReach)
-                    return -1;
-    
-                // re-initialize the steps to the amount
-                // of steps to reach maxReach from position i.
-                step = maxReach - i;
-            }
-        }
-    
-        return -1;
-            
+    if(n<=1){   //already at the end
+        return 0;
     }
+    
+    if(arr[0]==0){  //can't go anywhere
+        return -1;
+    }
+    
+    // stores the maximal
+    // reachable index in the array.
+    int maxReach = arr[0];
+
+    // stores the number of steps
+    // we can still take
+    int step = arr[0];
+
+    // stores the number of jumps
+    // necessary to reach that maximal
+    // reachable position.
+    int jump = 1;
+    
+    int i = 1;
+    for (i = 1; i < n; i++) {
+        // Check if we have reached the end of the array
+        if (i == n - 1)
+            return jump;
+
+        // updating maxReach
+        maxReach = max(maxReach, i + arr[i]);
+
+        // we use a step to get to the current index
+        step--;
+
+        // If no further steps left
+        if (step == 0) {
+            // we must have used a jump
+            jump++;
+
+            // Check if the current index/position or lesser index
+            // is the maximum reach point from the previous indexes
+            if (i >= maxReach)
+                return -1;
+
+            // re-initialize the steps to the amount
+            // of steps to reach maxReach from position i.
+            step = maxReach - i;
+        }
+    }
+
+    return -1;
+        
+}
