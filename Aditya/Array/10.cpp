3a70ce4cf921e708bd4d1d73aaf10c0ef9af1539
Aditya Prakash <prakashadityaap@gmail.com>
2021-05-31 12:20:22 +0530

    q10

--- Aditya/Array/10.cpp
+++ Aditya/Array/10.cpp
@@ -1,9 +1,30 @@
+//modifying the array
 int findDuplicate(vector<int>& nums) {
     sort(nums.begin(), nums.end());
     int ans = 0;
     for(int i = 0; i<nums.size()-1; i++){
         if(nums[i] == nums[i+1]){
             ans = nums[i];
+        }
+    }
+    return ans;
+}
+//without modifying the array
+int findDuplicate2(vector<int>& nums) {
+    int n = nums.size() - 1;
+    int arr[n+1];
+    for(int i = 0; i<=n; i++){
+        arr[i] = 0;
+    }
+
+    for(int i = 0; i<=n; i++){
+        arr[nums[i]]++;
+    }
+
+    int ans = -1;
+    for(int i = 0; i<=n; i++){
+        if(arr[i]>=2){
+            ans = i;
         }
     }
     return ans;
