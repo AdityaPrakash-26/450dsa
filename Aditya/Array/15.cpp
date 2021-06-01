a1c11867c7c7319844cc6f97821e94577a8a6e8c
Aditya Prakash <prakashadityaap@gmail.com>
2021-06-01 11:16:20 +0530

    q15

---
+++ Aditya/Array/15.cpp
@@ -0,0 +1,8 @@
+/* brute force approach:
+ * Start at the right hand side of the array
+ * and compare ever element before it. If the
+ * element is smaller, do nothing. If bigger,
+ * increment the `ans` by one. Repeat for every
+ * element.
+ */
+
