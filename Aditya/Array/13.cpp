6585407eadc9ed5bbb539d82487ff1f0c58588b3
Aditya Prakash <prakashadityaap@gmail.com>
2021-06-01 10:37:30 +0530

    q13 bug fix

--- Aditya/Array/13.cpp
+++ Aditya/Array/13.cpp
@@ -10,10 +10,10 @@ vector<vector<int> > merge(vector<vector<int> >& intervals) {
         if(res[j][1]>=intervals[i][0]){ //first array's second element is larger than first element of second array i.e. interval is feasible
             if(res[j][1] < intervals[i][1]){ //but the same is smaller than 2nd element of 2nd array, hence we replace 1st's 2nd with 2nd's 2nd i.e. expand the interval
 				res[j][1] = intervals[i][1];
-			} else {
+			}
+        } else {
                 res.push_back(intervals[i]); //no overlap found, hence push an existing interval
                 j++;
-            }
         }
     }
     return res;
