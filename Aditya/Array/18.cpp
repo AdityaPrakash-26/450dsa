c57cb5cbdbc6b203899dcbcfdb2ce8f9a53b3e28
Aditya Prakash <prakashadityaap@gmail.com>
2021-06-03 09:06:31 +0530

    q19

--- Aditya/Array/18.cpp
+++ Aditya/Array/18.cpp
@@ -14,12 +14,13 @@ vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3){
         m2[B[i]]++;
     for (int i = 0; i < n3; i++)
         m3[C[i]]++;
-
-    // check if frequency of element greater than 0 in all
-    // three maps
-    for (int i = 0; i < n1; i++) {
-        if (m1[i] > 0 and m2[i] > 0 and m3[i] > 0)
+    
+    int smallest = n1 < n2 ? (n1 < n3 ? n1 : n3) : (n2 < n3 ? n2 : n3);
+    
+    for (int i = 0; i < smallest; i++) {
+        if(m1[i]>0 && m2[i]>0 && m3[i]>0){
             ans.push_back(A[i]);
-
-    return ans;
+        }
+    }
+return ans;
 }
