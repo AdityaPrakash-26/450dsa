11b1f0f136dd7be53041b9ab88f74ade3b77fed5
Aditya Prakash <prakashadityaap@gmail.com>
2021-06-14 18:25:46 +0530

    q21

--- Aditya/Array/21.cpp
+++ Aditya/Array/21.cpp
@@ -1,6 +1,54 @@
+#include <iostream>
 #include <vector>
 using namespace std;
 
-vector<int> factorial(int N){
-    
+#define MAX 500
+
+void factorial(int n){
+    int res[MAX];
+ 
+    // Initialize result
+    res[0] = 1;
+    int res_size = 1;
+ 
+    // Apply simple factorial formula n! = 1 * 2 * 3 * 4...*n
+    for (int x=2; x<=n; x++)
+        res_size = multiply(x, res, res_size);
+ 
+    cout << "Factorial of given number is \n";
+    for (int i=res_size-1; i>=0; i--)
+        cout << res[i];
+}
+ 
+// This function multiplies x with the number
+// represented by res[].
+// res_size is size of res[] or number of digits in the
+// number represented by res[]. This function uses simple
+// school mathematics for multiplication.
+// This function may value of res_size and returns the
+// new value of res_size
+int multiply(int x, int res[], int res_size)
+{
+    int carry = 0;  // Initialize carry
+ 
+    // One by one multiply n with individual digits of res[]
+    for (int i=0; i<res_size; i++)
+    {
+        int prod = res[i] * x + carry;
+ 
+        // Store last digit of 'prod' in res[] 
+        res[i] = prod % 10; 
+ 
+        // Put rest in carry
+        carry  = prod/10;   
+    }
+ 
+    // Put carry in res and increase result size
+    while (carry)
+    {
+        res[res_size] = carry%10;
+        carry = carry/10;
+        res_size++;
+    }
+    return res_size;
 }
