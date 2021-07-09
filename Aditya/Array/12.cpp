4b40fd2f7f5f35db6c2416a507a2134347bd5ed8
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-09 10:09:06 +0530

    q12, added minSubarraySum

--- Aditya/Array/12.cpp
+++ Aditya/Array/12.cpp
@@ -1,5 +1,6 @@
+#include <iostream>
+using namespace std;
 int maxSubarraySum(int arr[], int n){
-        
         int sum = INT_MIN;
         int maxSumHere = 0;
         for(int i = 0; i<n; i++) {
@@ -11,6 +12,22 @@ int maxSubarraySum(int arr[], int n){
                 maxSumHere = 0;
             }
         }
-        return sum;
-            
+        return sum;            
+}
+
+int minSubarraySum(int arr[], int n){
+    int sum = INT_MAX;
+    int minSumHere = INT_MAX;
+
+    for(int i = 0; i<n; i++){
+        if(minSumHere>0){
+            minSumHere = arr[i];
+        } else {
+            minSumHere += arr[i];
+        }
+        if(minSumHere<sum){
+            sum = minSumHere;
+        }
     }
+    return sum;
+}
