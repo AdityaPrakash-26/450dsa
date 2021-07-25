deb8acd09638bd5aca1b776a78a12bb5af84f7b1
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-25 09:57:23 +0530

    q5

---
+++ Aditya/BinaryTrees/5.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class TreeNode {
+public:
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x){
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+void recursiveInorder(TreeNode* root){
+    if(root==nullptr){
+        return;
+    }
+
+    recursiveInorder(root->left);
+    cout<<root->data;
+    recursiveInorder(root->right);
+    return;
+}
