cbae88bfcfe2e3b3d5c0c6d77b8fa50dc10716b1
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-25 09:59:13 +0530

    q6

---
+++ Aditya/BinaryTrees/6.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class TreeNode {
+public:
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x){
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+void recursivePreorder(TreeNode* root){
+    if(root == nullptr){
+        return;
+    }
+
+    cout<<root->data;
+    recursivePreorder(root->left);
+    recursivePreorder(root->right);
+}
