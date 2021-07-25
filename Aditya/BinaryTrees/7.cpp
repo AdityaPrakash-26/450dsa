88bdafe7c57e5778f21b81a8b9342e0f56cf3966
Aditya Prakash <prakashadityaap@gmail.com>
2021-07-25 10:10:52 +0530

    q7

---
+++ Aditya/BinaryTrees/7.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class TreeNode {
+public:
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x){
+        data = x;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+void recursivePostorder(TreeNode* root){
+    if(root == nullptr){
+        return;
+    }
+
+    recursivePostorder(root->left);
+    recursivePostorder(root->right);
+    cout<<root->data;
+    return;
+}
+
+int main() {
+    
+}
